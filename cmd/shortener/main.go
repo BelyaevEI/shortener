a14959220b447afafb7bdce99f0084ec61f780a5
egorb <egor.belyaev95@yandex.ru>
2023-06-02 20:36:18 +0300

    the first increment is completed

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,3 +1,48 @@
 package main
 
-func main() {}
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", replaceURLPOST)    //Обработчик для POST запроса
+	http.HandleFunc("/{id}", replaceURLGET) //Обработчик для GET зарпоса
+
+	err := http.ListenAndServe(`:8080`, nil)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func replaceURLPOST(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	body := make([]byte, r.ContentLength)
+	_, err := r.Body.Read(body)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	shortURL := "http://localhost:8080/EwHXdJfB"
+
+	w.Header().Set("Content-Type", "text/plain")
+	w.WriteHeader(http.StatusCreated)
+	fmt.Fprintf(w, "%s\n", shortURL)
+}
+
+func replaceURLGET(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	originalURL := "https://practicum.yandex.ru/"
+
+	w.Header().Set("Location", originalURL)
+	w.WriteHeader(http.StatusTemporaryRedirect)
+}
