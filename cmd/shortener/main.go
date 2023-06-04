34badd279c161164d2ee5cd1ed1c43e9afafbd88
egorb <egor.belyaev95@yandex.ru>
2023-06-04 14:06:34 +0300

    third commit

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,48 +1,102 @@
 package main
 
 import (
-	"fmt"
+	"io"
+	"math/rand"
 	"net/http"
+	"strings"
+	"time"
 )
 
-func main() {
-	http.HandleFunc("/", replaceURLPOST)    //Обработчик для POST запроса
-	http.HandleFunc("/{id}", replaceURLGET) //Обработчик для GET зарпоса
+var short2long = make(map[string]string) //Словарь для получения полного URL по короткому
+var long2short = make(map[string]string) //Словарь для получения короткого URL по полному
 
-	err := http.ListenAndServe(`:8080`, nil)
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc(`/`, shortener)
+
+	err := http.ListenAndServe(`:8080`, mux)
 	if err != nil {
 		panic(err)
 	}
 }
 
-func replaceURLPOST(w http.ResponseWriter, r *http.Request) {
-	if r.Method != http.MethodPost {
+func shortener(w http.ResponseWriter, r *http.Request) {
+
+	switch r.Method {
+	case http.MethodPost:
+		replacePOST(w, r)
+	case http.MethodGet:
+		replaceGET(w, r)
+	default:
 		w.WriteHeader(http.StatusBadRequest)
-		return
 	}
-
-	body := make([]byte, r.ContentLength)
-	_, err := r.Body.Read(body)
-	if err != nil {
-		w.WriteHeader(http.StatusBadRequest)
-		return
-	}
-
-	shortURL := "http://localhost:8080/EwHXdJfB"
-
-	w.Header().Set("Content-Type", "text/plain")
-	w.WriteHeader(http.StatusCreated)
-	fmt.Fprintf(w, "%s\n", shortURL)
 }
 
-func replaceURLGET(w http.ResponseWriter, r *http.Request) {
-	if r.Method != http.MethodGet {
+func replacePOST(w http.ResponseWriter, r *http.Request) {
+
+	//Считать из тела запроса строку URL
+	longURL, err := io.ReadAll(r.Body)
+	if err != nil || string(longURL) == " " {
 		w.WriteHeader(http.StatusBadRequest)
 		return
 	}
 
-	originalURL := "https://practicum.yandex.ru/"
+	//Проверим наличие короткой ссылки по длинной, если ее нет
+	//то сгенерируем и запишем в словарь
+	if shortURL, ok := long2short[string(longURL)]; ok {
+		w.Header().Set("Content-Type", "text/plain")
+		w.WriteHeader(http.StatusCreated)
+		w.Write([]byte(shortURL))
+	} else {
+		short := generateRandomString(8)
+		shortURL = "localhost:8080/" + short
 
-	w.Header().Set("Location", originalURL)
-	w.WriteHeader(http.StatusTemporaryRedirect)
+		long2short[string(longURL)] = shortURL
+		short2long[short] = string(longURL)
+
+		w.WriteHeader(http.StatusCreated)
+		w.Write([]byte(shortURL))
+	}
+
+}
+
+func replaceGET(w http.ResponseWriter, r *http.Request) {
+
+	//получим ID из запроса
+	idLong := r.URL.Path[1:]
+	id := strings.Split(idLong, "/")[0]
+	if id == " " {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	//проверим по ID ссылку
+	if longURL, ok := short2long[id]; ok {
+		w.Header().Set("Location", longURL)
+		// } else {
+		// 	longURL = "https://practicum.yandex.ru/"
+		// 	w.Header().Set("Location", longURL)
+		// 	short2long[id] = longURL
+		w.Write([]byte(longURL))
+	}
+
+	w.WriteHeader(http.StatusOK)
+
+}
+
+func generateRandomString(length int) string {
+	// Задаем символы, из которых будет состоять случайная строка
+	charSet := "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+
+	// Инициализируем генератор случайных чисел
+	rand.Seed(time.Now().UnixNano())
+
+	// Генерируем случайную строку
+	result := make([]byte, length)
+	for i := 0; i < length; i++ {
+		result[i] = charSet[rand.Intn(len(charSet))]
+	}
+
+	return string(result)
 }
