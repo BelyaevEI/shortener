f9c4fc6154abea252070866ab81439bfc9a2c574
egorb <egor.belyaev95@yandex.ru>
2023-07-09 00:06:33 +0300

    work please

--- internal/route/route.go
+++ internal/route/route.go
@@ -2,6 +2,7 @@ package route
 
 import (
 	"github.com/BelyaevEI/shortener/internal/handlers"
+	m "github.com/BelyaevEI/shortener/internal/middlewares"
 	"github.com/go-chi/chi/v5"
 )
 
@@ -9,8 +10,8 @@ func New(h handlers.Handlers) *chi.Mux {
 
 	r := chi.NewRouter()
 	//Укажем middleware
-	r.Use(middlewares.Gzip)
-	r.Use(middlewares.Logger)
+	r.Use(m.Gzip)
+	r.Use(m.Logger)
 
 	r.Get("/{id}", h.ReplaceGET)
 	r.Post("/api/shorten", h.PostAPI)
