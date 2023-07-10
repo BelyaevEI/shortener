44ef5327681d169a0f79476eafa123ecbe622ee7
egorb <egor.belyaev95@yandex.ru>
2023-07-10 19:37:34 +0300

    10 increment is completed!

--- internal/database/database.go
+++ internal/database/database.go
@@ -4,12 +4,11 @@ import (
 	"database/sql"
 
 	"github.com/BelyaevEI/shortener/internal/config"
-	_ "github.com/jackc/pgx/v5/stdlib"
 )
 
 func Connect(cfg config.Parameters) (*sql.DB, error) {
 
-	db, err := sql.Open("pgx", cfg.DBStoragePath)
+	db, err := sql.Open("postgres", cfg.DBStoragePath)
 	if err != nil {
 		return nil, err
 	}
