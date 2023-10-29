bf8b9193c408f5a1b27715e6df28a59fd2d34005
BelyaevEI <egor.belyaev95@yandex.ru>
2023-10-29 16:20:22 +0300

    add test

--- internal/handlers/handlers_test.go
+++ internal/handlers/handlers_test.go
@@ -37,11 +37,11 @@ func TestReplacePOST(t *testing.T) {
 		},
 	}
 
-	// test2 := test{name: "Empty link in body",
-	// 	want: want{
-	// 		code: http.StatusBadRequest,
-	// 	},
-	// }
+	test2 := test{name: "Empty link in body",
+		want: want{
+			code: http.StatusBadRequest,
+		},
+	}
 
 	// Парсинг переменных окружения
 	cfg := config.ParseFlags()
@@ -92,46 +92,34 @@ func TestReplacePOST(t *testing.T) {
 		//Проверка тела ответа на пустоту
 		assert.NotEmpty(t, string(resBody))
 
-		// // создаём файл журнала профилирования памяти
-		// fmem, err := os.Create(`base.pprof`)
-		// if err != nil {
-		// 	panic(err)
-		// }
-		// defer fmem.Close()
-
-		// runtime.GC() // получаем статистику по использованию памяти
-		// if err := pprof.WriteHeapProfile(fmem); err != nil {
-		// 	panic(err)
-		// }
-
 	})
 
-	// t.Run(test2.name, func(t *testing.T) {
+	t.Run(test2.name, func(t *testing.T) {
 
-	// 	//Создаем тело запроса
-	// 	requestBody := strings.NewReader(" ")
+		//Создаем тело запроса
+		requestBody := strings.NewReader(" ")
 
-	// 	//Создаем сам запрос
-	// 	request := httptest.NewRequest(http.MethodPost, "/", requestBody)
+		//Создаем сам запрос
+		request := httptest.NewRequest(http.MethodPost, "/", requestBody)
 
-	// 	//Устанавливаем заголовок
-	// 	request.Header.Set("Content-Type", "text/plain")
+		//Устанавливаем заголовок
+		request.Header.Set("Content-Type", "text/plain")
 
-	// 	//Создаем рекордер для записи ответа
-	// 	responseRecorder := httptest.NewRecorder()
+		//Создаем рекордер для записи ответа
+		responseRecorder := httptest.NewRecorder()
 
-	// 	//Обрабатываем запрос
-	// 	h.ReplacePOST(responseRecorder, request)
+		//Обрабатываем запрос
+		h.ReplacePOST(responseRecorder, request)
 
-	// 	//Получаем ответ
-	// 	result := responseRecorder.Result()
+		//Получаем ответ
+		result := responseRecorder.Result()
 
-	// 	defer result.Body.Close()
-	// 	//Делаем проверки
-	// 	//Проверка ответа сервера
-	// 	assert.Equal(t, test2.want.code, result.StatusCode)
+		defer result.Body.Close()
+		//Делаем проверки
+		//Проверка ответа сервера
+		assert.Equal(t, test2.want.code, result.StatusCode)
 
-	// })
+	})
 }
 
 func BenchmarkReplacePOST(b *testing.B) {
